fee2507fb47f9e3dc4db22fb5506e04147ea9411
satcom-uhf <90838159+satcom-uhf@users.noreply.github.com>
2021-09-16 17:39:11 +0500

    Arduino sketch

---
+++ RemoteControl/code.ino
@@ -0,0 +1,66 @@
+int valcoderUp = 12;
+int valcoderDown = 10;
+int menu = 8;
+int abcd = 6;
+bool valcoderState = false;
+void setup(void)
+{
+   Serial.begin(9600);
+   pinMode(valcoderUp, OUTPUT);
+   pinMode(valcoderDown, OUTPUT);
+   pinMode(menu, OUTPUT);
+   pinMode(abcd, OUTPUT);
+}
+
+void clickButton(int pin)
+{
+   digitalWrite(pin, HIGH);
+   delay(10);
+   digitalWrite(pin, LOW);
+}
+
+void valCoderTick(int pin1, int pin2)
+{
+   valcoderState = !valcoderState;
+   digitalWrite(pin1, valcoderState);
+   delay(3);
+   digitalWrite(pin2, valcoderState);
+}
+
+void up()
+{
+   valCoderTick(valcoderUp, valcoderDown);
+}
+void down()
+{
+   valCoderTick(valcoderDown, valcoderUp);
+}
+
+void loop()
+{
+   byte ch;
+
+   if (Serial.available())
+   {
+      ch = Serial.read();
+      switch (ch)
+      {
+      case '+':
+         up();
+         Serial.println("Up");
+         break;
+      case '-':
+         down();
+         Serial.println("Down");
+         break;
+      case 'e':
+         clickButton(abcd);
+         Serial.println("ABCD");
+         break;
+      case 'm':
+         clickButton(menu);
+         Serial.println("MENU");
+         break;
+      }
+   }
+}
